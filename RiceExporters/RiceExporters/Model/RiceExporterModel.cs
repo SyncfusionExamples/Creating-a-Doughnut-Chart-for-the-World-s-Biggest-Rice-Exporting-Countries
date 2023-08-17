753fc731cf61271a7e85d00f2a912ef95f3b6e81
SaiyathAliFathima <103025761+SaiyathAliFathima@users.noreply.github.com>
2023-08-17 18:46:17 +0530

    Added the sample

---
+++ RiceExporters/RiceExporters/Model/RiceExporterModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiceExporters
+{
+    public class RiceExporterModel
+    {
+        public string Country { get; set; }
+        public Double Value { get; set; }
+
+        public RiceExporterModel(string country, Double value)
+        {
+            Country = country;
+            Value = value/1000;
+        }
+    }
+}
